34b44740a0949f35f30f4de8c8231aba34212991
Nelly Vladova <101873582+NellyVladova@users.noreply.github.com>
2022-04-12 14:48:18 +0300

    Add files via upload

---
+++ E02Encapsulation/P03ShoppingSpree/Main.java
@@ -0,0 +1,7 @@
+package E02Encapsulation.P03ShoppingSpree;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ E02Encapsulation/P04PizzaCalories/Main.java
@@ -0,0 +1,7 @@
+package E02Encapsulation.P04PizzaCalories;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
